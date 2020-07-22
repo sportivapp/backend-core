f4d9ed1ab11e922c1ff7570c0cfceef9be2c240e
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-22 19:21:45 +0700

    add user list route

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Create, Login } = require('../../controllers/user');
+const { Create, Login , List} = require('../../controllers/user');
 
 router.post('/user', Create);
 router.post('/user-login', Login);
+router.get('/user-list', List);
 
 module.exports = router;
