2d0081bc8fb5799597ebc90a91483a919ac004e1
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-22 20:47:11 +0700

    seperated test for user

---
+++ test/user.js
@@ -0,0 +1,30 @@
+module.exports = (chai, httpServer, expect) => {
+  
+  describe('GET /api/v1/user-list', () => {
+    it('Should return a list of users based on ecompany Id', async () => {
+      const res = await chai.request(httpServer)
+      .get('/api/v1/user-list')
+      .send({
+        ecompanyecompanyid: 1
+      });
+      expect(res.status).to.equal(200);
+      expect(res.body.data).to.not.be.undefined;
+    });
+  });
+
+  describe('POST /api/v1/user-login', () => {
+    it('Should return jwt token after successful login', async () => {
+      const res = await chai.request(httpServer)
+      .post('/api/v1/user-login')
+      .send({ 
+        email: 'nawakaraadmin@nawakara.com', 
+        password: 'emtivnawakaraadmin' 
+      });
+
+      expect(res.status).to.equal(200);
+      expect(res.body.data).to.not.be.undefined;
+      expect(res.body.data).to.have.property('token');
+    });
+  });
+
+}
