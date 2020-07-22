464d3d7b0dce7e290b15913e376773b7e9f834bc
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-22 14:00:07 +0700

    add swaggerDoc

---
+++ src/swaggerDoc.js
@@ -0,0 +1,111 @@
+const   swaggerJsDoc= require('swagger-jsdoc'),
+        swaggerUi   = require('swagger-ui-express')
+
+const routes = require('./routes/v1');
+
+
+// paths: {
+//   "/users": {
+//     "post": {
+//       "summary": "Add user",
+//       "tags": [
+//         "Users"
+//       ],
+//       "description": "add user to system",
+//       "parameters": [
+//           {
+//             "name": "user",
+//             "in": "body",
+//             "description": "add user",
+//             "schema": {
+//               "$ref": "#/definitions/User"
+//             }
+//           }
+//         ],
+//       "produces": [
+//         "application/json"
+//       ],
+//       "responses": {
+//         "200": {
+//           "description": "Login Success",
+//           "schema": {
+//             "$ref": "#/definitions/User"
+//           }
+//         }
+//       }
+//     }
+//   }
+
+// "definitions
+//   "User": {
+//     "properties": {
+//       "nik": {
+//         "type": "integer"
+//       },
+//       "name": {
+//           "type": "string"
+//       },
+//       "email": {
+//           "type": "string"
+//       },
+//       "password": {
+//           "type": "string"
+//       },
+//       "mobileNumber": {
+//           "type": "string"
+//       }
+//     }
+//   },
+// }
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'Emtiv Backend API',
+            version: '1.0.0',
+            description: 'Emtiv Backend API Description',
+        },
+        host: 'localhost:3000',
+        basePath: '/api/v1',
+        tags: [
+            {
+            "name": "Users",
+            "description": "API for users in the system"
+            }
+
+        ],
+        schemes: [
+            "http",
+            "https"
+        ],
+        consumes: [
+            "application/json"
+        ],
+        produces: [
+            "application/json"
+        ],
+        securityDefinitions: {
+            "ApiKeyAuth":{
+                "type": "apiKey",
+                "in": "headers",
+                "name": "authorization"
+            }
+        },
+    },
+    // path to the API docs
+    apis: ['./src/routes/v1/*.js'],
+  };
+
+// setup swagger
+const swaggerDocs = swaggerJsDoc(options);
+
+module.exports = (app) => {
+    // make swagger docs
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    app.use('/api/v1', routes);
+    // serve swagger
+    app.get('/swagger.json', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerDocs);
+    });
+}
+
