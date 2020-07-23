1991cf9b4cb44b7a48fdf1cfa7105e94f7f3fd13
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-23 17:57:25 +0700

    create delete user

---
+++ src/controllers/user/Delete.js
@@ -0,0 +1,33 @@
+const userService = require('../../services/userService');
+
+module.exports = async (req, res, next) => {
+
+    try {
+        
+        const user = req.user;
+        const euserId= req.body.euserid;
+
+        if (user.permission !== 10) {
+            return res.status(401).json({
+                data: 'You cannot register employees'
+            })
+        }
+
+        // return 1 for true , 0 for false
+        const deleteUser = await userService.deleteUserById(euserId);
+
+        const data = {
+            isDeleted: (deleteUser) ? true : false,
+            message: (deleteUser) ? "User Successfully Delete!" : "User Not Found!"
+        }
+
+        return res.status(200).json({
+            data: data
+        })
+
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
+
+}
