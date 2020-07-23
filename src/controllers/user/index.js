b5583703049338d40b1691d2d496c94e8692215c
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-23 15:07:50 +0700

    add change password

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,6 +1,7 @@
 const Create = require('./Create');
 const Login = require('./Login');
 const List = require('./List');
-const Template = require('./Template')
+const Template = require('./Template');
+const ChangePassword = require('./ChangePassword');
 
-module.exports = { Create, Login, List, Template };
+module.exports = { Create, Login, List, Template , ChangePassword};
