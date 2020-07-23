58d682506918fda8ea82c14773122b685b1b71ab
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-23 17:57:35 +0700

    add delete

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -3,5 +3,6 @@ const Login = require('./Login');
 const List = require('./List');
 const Template = require('./Template');
 const ChangePassword = require('./ChangePassword');
+const Delete = require('./Delete');
 
-module.exports = { Create, Login, List, Template , ChangePassword};
+module.exports = { Create, Login, List, Template , ChangePassword, Delete};
