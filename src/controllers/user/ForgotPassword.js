85c5ccbec4b280c3364f0682a6bed3f5364f33ce
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-24 17:53:18 +0700

    create forgot password

---
+++ src/controllers/user/ForgotPassword.js
@@ -0,0 +1,23 @@
+const userService = require('../../services/userService');
+
+module.exports = async (req, res, next) => {
+
+    try {
+
+        const userEmail = req.body.userEmail; 
+
+        await userService.sendForgotPasswordLink( userEmail );
+
+        const data = {
+            message: "Successfully Send Password"
+        }
+
+        return res.status(200).json({
+            data: data
+        });
+        
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
+}
