9e2352827da8fe20175e435404c3818469a7615c
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-24 17:59:45 +0700

    import forgot password

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -4,5 +4,6 @@ const List = require('./List');
 const Template = require('./Template');
 const ChangePassword = require('./ChangePassword');
 const Delete = require('./Delete');
+const ForgotPassword = require('./ForgotPassword');
 
-module.exports = { Create, Login, List, Template , ChangePassword, Delete};
+module.exports = { Create, Login, List, Template , ChangePassword, Delete, ForgotPassword};
