a4df2dfe7c41153121b3576a407053d0db26de97
Surya <suryaboyz04@gmail.com>
2020-07-24 12:24:02 +0700

    add user seed

--- src/db/seeds/5users.js
+++ src/db/seeds/5users.js
@@ -10,6 +10,15 @@ exports.seed = (knex, Promise) => knex('euser').del()
           euserpassword: '$2y$10$UzQchZo/FrFs4UMzrRPp2OXj.tjV495wGW977M4SSczXqdVUWRaKu',
           eusermobilenumber: '987654321',
           ecompanyecompanyid: 1
-        }
+        },
+        {
+          euserpermission: 1,
+          eusernik: 111111111,
+          eusername: 'nawakarauser',
+          euseremail: 'nawakarauser@nawakara.com',
+          euserpassword: '$2y$10$NpWABbq3oj8NWtpegmNIU..MWwX8ufWfhv03NyHuaULXXKOQa777G',
+          eusermobilenumber: '11111111',
+          ecompanyecompanyid: 1
+        },
       ]
     ));
