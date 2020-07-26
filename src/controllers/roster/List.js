1cf2775b183f235a59273e837cac05596b168b73
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-26 22:46:14 +0700

    add list for roster

---
+++ src/controllers/roster/List.js
@@ -0,0 +1,25 @@
+const RosterService = require('../../services/rosterService');
+
+module.exports = async (req, res, next) => {
+
+    try {
+
+        const user = req.user;
+
+        if (user.permission !== 8) {
+            return res.status(401).json({
+                data: 'You cannot see rosters'
+            })
+        }
+
+        const rosters = await RosterService.getAllRosters();
+
+        return res.status(200).json({
+            data: rosters
+        });
+        
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
+}
