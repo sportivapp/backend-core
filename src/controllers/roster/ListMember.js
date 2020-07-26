e856f231308ddd4b87706d65883da1fff7803b94
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-26 22:46:21 +0700

    add list member

---
+++ src/controllers/roster/ListMember.js
@@ -0,0 +1,26 @@
+const RosterService = require('../../services/rosterService');
+
+module.exports = async (req, res, next) => {
+
+    try {
+
+        const user = req.user;
+        const rosterId = req.body.rosterId;
+
+        if (user.permission !== 8) {
+            return res.status(401).json({
+                data: 'You cannot see roster members'
+            })
+        }
+
+        const members = await RosterService.getAllMemberById( rosterId );
+
+        return res.status(200).json({
+            data: members
+        });
+        
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
+}
