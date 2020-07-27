b8a31ec746b1b03d3ff07b0ff74927bd631d9431
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-27 23:07:34 +0700

    add announcement index controller

---
+++ src/controllers/announcement/index.js
@@ -0,0 +1,7 @@
+const Create = require('./Create');
+const Update = require('./Update');
+const View = require('./View');
+const List = require('./List');
+const Delete = require('./Delete');
+
+module.exports = { Create, Update, View, List, Delete };
