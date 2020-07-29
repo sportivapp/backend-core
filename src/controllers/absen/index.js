e818736e67c6ca42cc12b0d5c2d9b6f3e8fdfff9
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-29 15:57:59 +0700

    add absen index controller

---
+++ src/controllers/absen/index.js
@@ -0,0 +1,7 @@
+const Create = require('./Create');
+const List = require('./List');
+const ListById = require('./ListById');
+const Update = require('./Update');
+const Delete = require('./Delete');
+
+module.exports = { Create, List, ListById, Update, Delete };
