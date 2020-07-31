471ed4f38421f39a680ba9aca86118985370cc0f
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-07-31 20:35:37 +0700

    modified-seed-files-to-run-properly

--- src/db/seeds/12-permit.js
+++ src/db/seeds/12-permit.js
@@ -1,11 +1,11 @@
-exports.seed = (knex, Promise) => knex('epermit').del()
+exports.seed = (knex) => knex('epermit').del()
     .then(() =>
         knex('epermit').insert(
             [
                 {
                     epermitdescription: 'nawakara permit staff',
                     epermitstartdate: '2020-12-12',
-                    epermitenddate: '2020-24-12',
+                    epermitenddate: '2020-12-24',
                     epermitstatus: 0,
                     euseruserid: 5,
                     epermitcreateby: 4
@@ -13,16 +13,16 @@ exports.seed = (knex, Promise) => knex('epermit').del()
                 {
                     epermitdescription: 'nawakara permit danru',
                     epermitstartdate: '2020-12-12',
-                    epermitenddate: '2020-24-12',
-                    epermitstatus: 0,
+                    epermitenddate: '2020-12-24',
+                    epermitstatus: 1,
                     euseruserid: 4,
                     epermitcreateby: 4
                 },
                 {
                     epermitdescription: 'nawakara permit pm',
                     epermitstartdate: '2020-12-12',
-                    epermitenddate: '2020-24-12',
-                    epermitstatus: 0,
+                    epermitenddate: '2020-12-24',
+                    epermitstatus: 2,
                     euseruserid: 3,
                     epermitcreateby: 4
                 }
