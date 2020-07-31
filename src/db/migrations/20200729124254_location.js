c6b63007a3dce001e3b77f4fd969e20cd347ee3c
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-31 15:12:06 +0700

    remove unnecessary comment

--- src/db/migrations/20200729124254_location.js
+++ src/db/migrations/20200729124254_location.js
@@ -17,7 +17,6 @@ exports.up = (knex, Promise) => knex.schema.createTable('elocation', t => {
     t.timestamp('elocationdeletetime', false);
     t.integer('elocationdeletestatus').defaultTo(0);
     t.integer('elocationtablestatus').notNullable().defaultTo(1);
-    // t.integer('edeviceedeviceid').notNullable().references('edevice.edeviceid');
     t.integer('edeviceedeviceid').notNullable().references('edevice.edeviceid');
   });
   
