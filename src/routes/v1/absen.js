0026c397bd764aed2cbf37b0f2aca11fa290502d
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-07-31 15:12:30 +0700

    use temporary abse route

--- src/routes/v1/absen.js
+++ src/routes/v1/absen.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { Create, List, ListById, Update, Delete } = require('../../controllers/absen')
 const auth = require('../../middlewares/authentication');
 
-router.post('/absen', auth.authenticateToken, Create);
+// router.post('/absen', auth.authenticateToken, Create);
+router.post('/absen', Create); // sementara untuk tes api mobile
 router.get('/absen-list/:userId', auth.authenticateToken, ListById);
 router.get('/absen-list', auth.authenticateToken, List);
 router.put('/absen/:absenId', auth.authenticateToken, Update);
