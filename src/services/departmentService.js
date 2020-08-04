1e0276ea966aa6c163c0c7ff610d3316d9720d09
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-04 22:00:48 +0700

    add validation if company not found

--- src/services/departmentService.js
+++ src/services/departmentService.js
@@ -1,13 +1,24 @@
 const Department = require('../models/Department')
+const Company = require('../models/Company')
 const ServiceHelper = require('../helper/ServiceHelper')
 
 const departmentService = {}
 
 departmentService.getAllDepartmentbyCompanyId = async (page, size, companyId) => {
 
+    if(companyId) {
+        const result = await departmentService.getCompanyByCompanyId(companyId)
+
+        if( !result ) return
+    }
+
     const departmentPage = await Department.query().select().where('ecompanyecompanyid', companyId).page(page, size)
 
     return ServiceHelper.toPageObj(page, size, departmentPage)
+}
+
+departmentService.getCompanyByCompanyId = async (companyId) => {
+    return Company.query().select().where('ecompanyid', companyId).first()
 }
 
 departmentService.createDepartment = async (departmentDTO) => {
