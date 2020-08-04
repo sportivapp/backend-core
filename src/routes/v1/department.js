f4d2ee24af04b612334c3fa2b0faeb2a73c6ba8e
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-04 21:27:12 +0700

    remove company id on params

--- src/routes/v1/department.js
+++ src/routes/v1/department.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../../controllers/department')
 const auth = require('../../middlewares/authentication');
 
-router.get('/department/:companyId', auth.authenticateToken, controller.getAllDepartmentbyCompanyId);
+router.get('/department', auth.authenticateToken, controller.getAllDepartmentbyCompanyId);
 router.post('/department', auth.authenticateToken, controller.createDepartment);
 router.put('/department/:departmentId', auth.authenticateToken, controller.updateDepartment);
 router.delete('/department/:departmentId', auth.authenticateToken, controller.deleteDepartment);
