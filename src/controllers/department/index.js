07f0d004811fb225dfccdab7706a1b3f8e0e2fb3
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-04 19:39:54 +0700

    add department controller

---
+++ src/controllers/department/index.js
@@ -0,0 +1,113 @@
+const departmentService = require('../../services/departmentService')
+const ResponseHelper = require('../../helper/ResponseHelper')
+
+const controller = {}
+
+function isUserValid( user ) {
+    return user.permission !== 10
+}
+
+controller.getAllDepartmentbyCompanyId = async (req, res, next) => {
+    
+    const user = req.user
+
+    if (isUserValid(user)) 
+        return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const { companyId } = req.params
+    const { page, size } = req.query
+
+    try {
+        const pageObj = await departmentService.getAllDepartmentbyCompanyId(parseInt(page), parseInt(size), companyId)
+        if (!pageObj)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging))
+    } catch (e) {
+        next(e)
+    }
+
+}
+
+controller.createDepartment = async (req, res, next) => {
+    
+    const user = req.user
+
+    if (isUserValid(user)) 
+        return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const { departmentName, departmentDescription, departmentSuperiorId, companyId } = req.body
+
+    try {
+
+        const departmentDTO = {
+            edepartmentname: departmentName,
+            edepartmentdescription: departmentDescription,
+            edepartmentcreateby: user.sub,
+            edepartmentsuperiorid: departmentSuperiorId,
+            ecompanyecompanyid: companyId
+        }
+
+        const result = await departmentService.createDepartment(departmentDTO)
+        if (!result)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+
+    } catch (e) {
+        next(e)
+    }
+
+}
+
+controller.updateDepartment = async (req, res, next) => {
+    
+    const user = req.user
+
+    if (isUserValid(user)) 
+        return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const { departmentId } = req.params
+    const { departmentName, departmentDescription, departmentSuperiorId } = req.body
+
+    try {
+
+        const departmentDTO = {
+            edepartmentname: departmentName,
+            edepartmentdescription: departmentDescription,
+            edepartmentchangeby: user.sub,
+            edepartmentchangetime: Date.now(),
+            edepartmentsuperiorid: departmentSuperiorId
+        }
+
+        const result = await departmentService.updateDepartment(departmentId, departmentDTO)
+        if (!result)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.deleteDepartment = async (req, res, next) => {
+    
+    const user = req.user
+
+    if (isUserValid(user)) 
+        return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const { departmentId } = req.params
+
+    try {
+
+        const result = await departmentService.deleteDepartment(departmentId)
+        if (!result)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+
+    } catch (e) {
+        next(e)
+    }
+
+}
+
+module.exports = controller
