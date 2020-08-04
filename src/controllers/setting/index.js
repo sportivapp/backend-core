3d5597601f808cac84383838c5375c34ed69dbda
Surya <suryaboyz04@gmail.com>
2020-08-04 18:48:16 +0700

    setting controller

---
+++ src/controllers/setting/index.js
@@ -0,0 +1,24 @@
+const SettingService = require('../../services/settingService');
+const ResponseHelper = require('../../helper/ResponseHelper');
+
+const controller = {}
+
+function isUserValid(user) {
+    return user.permission === 10
+}
+
+controller.getModules = async (req, res, next) => {
+
+    if(!isUserValid(req.user))
+        return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const { companyId } = req.params
+
+    try {
+        const result = await SettingService.getModules(companyId);
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+
+}
