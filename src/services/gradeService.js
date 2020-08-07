2de206089c323cae78034c5cd943fbe7b2094e52
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-07 22:24:07 +0700

    add userpositionmapping service

--- src/services/gradeService.js
+++ src/services/gradeService.js
@@ -1,5 +1,7 @@
 const Grade = require('../models/Grades')
+const User = require('../models/User')
 const Company = require('../models/Company')
+const UserPositionMapping = require('../models/UserPositionMapping')
 const ServiceHelper = require('../helper/ServiceHelper')
 
 const gradeService = {}
@@ -48,6 +50,71 @@ gradeService.deleteGradeById = async (gradeId) => {
         .delete()
         .where('egradeid', gradeId)
         .then(rowsAffected => rowsAffected === 1)
+}
+
+gradeService.saveUserPositions = async (userId, positionIds, loggedInUser) => {
+
+    //accepting model [{'id': 1, 'deleted': false/true}]
+    
+    const user = await User.query().findById(userId)
+
+    if(!user)
+        return
+
+    const deletedPositionIds = positionIds.filter(position => position.deleted)
+        .map(position => position.id)
+
+    const insertedPositionIds = positionIds.filter(position => !position.deleted)
+        .map(position => position.id)
+
+    const deleteRelations = UserPositionMapping.query()
+        .patch({
+            edeletestatus: true,
+            eassigndeleteby: loggedInUser.sub,
+            eassigndeletetime: Date.now()
+        })
+        .where('eusereuserid', userId)
+        .whereIn('egradeegradeid', deletedPositionIds)
+
+    const undoDeletedPositions = UserPositionMapping.query()
+        .patch({ edeletestatus: false })
+        .where('edeletestatus', true)
+        .where('eusereuserid', userId)
+        .whereIn('egradeegradeid', insertedPositionIds)
+        .returning('egradeegradeid')
+
+    const existedPositions = UserPositionMapping.query()
+        .select()
+        .where('eusereuserid', userId)
+        .returning('egradeegradeid')
+
+    return Promise.all([deleteRelations, undoDeletedPositions, existedPositions])
+        .then(resultArr => {
+            console.log('passed')
+            return resultArr[2]
+        })
+        .then(existedRelations => {
+            console.log('passed')
+            const freshRelations = insertedPositionIds
+            .filter(positionId => !existedRelations.find(relation => relation.egradeegradeid === positionId))
+            .map(positionId => ({
+                eusereuserid: parseInt(userId),
+                egradeegradeid: positionId,
+                eassigncreateby: loggedInUser.sub
+            }))
+            return [freshRelations, existedRelations]
+        })
+        .then(relationArr => {
+
+            return UserPositionMapping.query().insert(relationArr[0])
+            .then(freshRelations => {
+                let resultArr = []
+                resultArr.push(...relationArr[1])
+                resultArr.push(...freshRelations)
+                return resultArr
+            })
+        })
+
 }
 
 module.exports = gradeService
