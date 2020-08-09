b76011946259e6cc8f25872505b71747bc7059b8
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-09 15:11:22 +0700

    change migration

--- src/db/migrations/20200807150829_user_position_mapping.js
+++ src/db/migrations/20200807150829_user_position_mapping.js
@@ -1,10 +1,13 @@
 exports.up = (knex, Promise) => knex.schema.createTable('euserpositionmapping', t => {
     t.integer('eusereuserid').notNullable().references('euser.euserid')
     t.integer('egradeegradeid').notNullable().references('egrade.egradeid')
-    t.bigInteger('eassigncreatetime').notNullable().defaultTo(Date.now())
-    t.integer('eassigncreateby').notNullable()
-    t.bigInteger('eassigndeletetime')
-    t.integer('eassigndeleteby')
+    t.bigInteger('euserpositionmappingcreatetime').notNullable().defaultTo(Date.now())
+    t.integer('euserpositionmappingcreateby').notNullable()
+    t.integer('euserpositionmappingchangeby');
+    t.bigInteger('euserpositionmappingchangetime');
+    t.bigInteger('euserpositionmappingdeletetime')
+    t.boolean('euserpositionmappingdeletestatus').defaultTo(false);
+    t.integer('euserpositionmappingdeleteby')
     t.boolean('edeletestatus').defaultTo(false)
 });
 
