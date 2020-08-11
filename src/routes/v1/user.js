c5816039e96dd6a005fbf277845af7885fb4cfeb
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-11 18:05:35 +0700

    change routes implementation

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -14,13 +14,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 const { routes } = require('../../constant')
 
-router.post('/user', auth.authenticateToken, upload.single('employee'), userController.registerEmployees);
+router.post( routes.user.register, auth.authenticateToken, upload.single('employee'), userController.registerEmployees);
 router.post( routes.user.create, auth.authenticateToken, userController.createUser);
-router.post(routes.user.login, userController.login);
-router.post(routes.user.forgot, userController.forgotPassword);
-router.get('/user/:companyId', auth.authenticateToken, userController.getAllUserByCompanyId);
-router.get('/user-import-template', userController.importTemplate);
+router.post( routes.user.login, userController.login);
+router.post( routes.user.forgot, userController.forgotPassword);
+router.get( routes.user.list, auth.authenticateToken, userController.getAllUserByCompanyId);
+router.get( routes.user.import, userController.importTemplate);
 router.put( routes.user.password, auth.authenticateToken, userController.changePassword);
-router.delete('/user/:userId', auth.authenticateToken, userController.deleteUserById);
+router.delete( routes.user.remove , auth.authenticateToken, userController.deleteUserById);
 
 module.exports = router.expressRouter;
