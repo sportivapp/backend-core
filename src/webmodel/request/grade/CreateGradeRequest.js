877ba9396b586242a33839142d1e7c7fca17473a
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-14 14:48:58 +0700

    change webmodel

--- src/webmodel/request/grade/CreateGradeRequest.js
+++ src/webmodel/request/grade/CreateGradeRequest.js
@@ -4,7 +4,8 @@ const schema = Joi.object({
     name: Joi.string().max(255).required(),
     description: Joi.string().max(255).required(),
     companyId: Joi.number().positive().required(),
-    superiorId: Joi.number().positive()
+    superiorId: Joi.number().positive(),
+    departmentId: Joi.number().positive().required()
 })
 
 module.exports = schema
