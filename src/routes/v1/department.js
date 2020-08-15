540f558f8dcb3da9c64daccedde0340011052d2b
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-15 17:46:37 +0700

    change routes

--- src/routes/v1/department.js
+++ src/routes/v1/department.js
@@ -3,8 +3,8 @@ const controller = require('../../controllers/department')
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.get( routes.department.department, auth.authenticateToken, controller.getAllDepartmentbyCompanyId);
-router.post( routes.department.department, auth.authenticateToken, controller.createDepartment);
+router.get( routes.department.list, auth.authenticateToken, controller.getAllDepartmentbyCompanyId);
+router.post( routes.department.list, auth.authenticateToken, controller.createDepartment);
 router.put( routes.department.id, auth.authenticateToken, controller.updateDepartment);
 router.delete( routes.department.id, auth.authenticateToken, controller.deleteDepartment);
 
