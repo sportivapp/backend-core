e83abeaba68890ed0eda41f3792bff1094e4d01f
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-08-16 12:02:11 +0700

    change-error-code-when-token-expired

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
+const ResponseHelper = require('../helper/ResponseHelper')
 
 exports.authenticateToken = async (req, res, next) => {
 
@@ -9,7 +10,7 @@ exports.authenticateToken = async (req, res, next) => {
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, user) {
         if (err) { 
-            return res.status(403).json('Unauthorized access');
+            return res.status(401).json("You need to log in first.");
         }
         req.user = user;
         next();
