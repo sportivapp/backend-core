730b884f764fe316813eb8644e89817ea555e1fe
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-08-18 14:54:24 +0700

    fix department webmodel

--- src/webmodel/request/department/UpdateDepartmentRequest.js
+++ src/webmodel/request/department/UpdateDepartmentRequest.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const schema = Joi.object({
     departmentName: Joi.string().max(255).required(),
     departmentDescription: Joi.string().max(255).required(),
-    departmentSuperiorId: Joi.number().required()
+    departmentSuperiorId: Joi.number().positive().allow(null)
 })
 
 module.exports = schema
