c6a1dbdb7d67d61eef4865111dbe7fe65a05fc2e
Surya <suryaboyz04@gmail.com>
2020-08-21 21:38:50 +0700

    add test for update module name

--- test/setting.js
+++ test/setting.js
@@ -18,4 +18,26 @@ module.exports = (chai, httpServer, expect) => {
       });
     });
 
+    describe('PUT /api/v1/setting/company-module/:companyId', () => {
+      it('Should return success update', async () => {
+        const login = await chai.request(httpServer)
+        .post('/api/v1/user-login')
+        .send({ 
+          email: 'nawakaraadmin@nawakara.com', 
+          password: 'emtivnawakaraadmin' 
+        });
+  
+        const res = await chai.request(httpServer)
+        .put('/api/v1/setting/company-module/1')
+        .set('authorization', login.body.data)
+        .send({
+          moduleId: 1,
+          moduleName: 'Perusahaan'
+      });
+        
+        expect(res.status).to.equal(200);
+        expect(res.body.data).to.not.be.undefined;
+      });
+    });
+
 }
