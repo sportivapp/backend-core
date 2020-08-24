6088995fed39b70b69f05737ee7eab5462cbb879
Surya <suryaboyz04@gmail.com>
2020-08-25 01:51:36 +0700

    file create time

--- src/db/seeds/000-file.js
+++ src/db/seeds/000-file.js
@@ -7,7 +7,8 @@ exports.seed = (knex, Promise) => knex('efile').del()
           efilename: 'important announcement.png',
           efilepath: '/opt/emtiv-backend/uploads/announcement/1/important announcement.png',
           efiletype: 'image/png',
-          efilecreateby: 0
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
         }
       ]
 ));
