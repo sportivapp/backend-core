8cda533b82222259bab2e1e06420ddf9b01c02ec
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-08-30 21:43:52 +0700

    change the naming

--- src/routes/v1/mobileExperience.js
+++ src/routes/v1/mobileExperience.js
@@ -4,10 +4,10 @@ const experienceController = require('../../controllers/mobileExperience');
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.post(routes.mobileExperience.list, auth.authenticateToken, experienceController.createExperience);
-router.get(routes.mobileExperience.list, auth.authenticateToken, experienceController.getExperienceList);
-router.get(routes.mobileExperience.id, auth.authenticateToken, experienceController.getExperienceById);
-router.put(routes.mobileExperience.id, auth.authenticateToken, experienceController.editExperience);
-router.delete(routes.mobileExperience.id, auth.authenticateToken, experienceController.deleteExperience);
+router.post(routes.experience.list, auth.authenticateToken, experienceController.createExperience);
+router.get(routes.experience.list, auth.authenticateToken, experienceController.getExperienceList);
+router.get(routes.experience.id, auth.authenticateToken, experienceController.getExperienceById);
+router.put(routes.experience.id, auth.authenticateToken, experienceController.editExperience);
+router.delete(routes.experience.id, auth.authenticateToken, experienceController.deleteExperience);
 
 module.exports = router;
