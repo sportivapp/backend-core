7feacabadb217f52d338eb1ee4365b75c8a5f07c
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-01 16:51:36 +0700

    add new routes

--- src/routes/v1/mobileUser.js
+++ src/routes/v1/mobileUser.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/mobileUser');
 const auth = require('../../middlewares/authentication');
+const { routes } = require('../../constant')
 
 router.post('/user', controller.createUser);
 router.post('/user-login', controller.login);
+router.put( routes.user.coach, auth.authenticateToken, controller.updateUserCoachData);
 router.post('/user-profile', auth.authenticateToken, controller.getUserById);
 router.get('/user-self-profile', auth.authenticateToken, controller.getSelf);
 router.put('/user', auth.authenticateToken, controller.updateUser);
