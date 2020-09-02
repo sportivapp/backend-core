96be0a63574853cc429b4377a4166638a3a49790
Surya <suryaboyz04@gmail.com>
2020-09-02 21:24:07 +0700

    Update CreateExperienceRequest.js

--- src/webmodel/request/experience/CreateExperienceRequest.js
+++ src/webmodel/request/experience/CreateExperienceRequest.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const schema = Joi.object({
     name: Joi.string().max(65).required(),
     startDate: Joi.number().positive().required(),
-    endDate: Joi.number(),
+    endDate: Joi.number().positive().allow(0),
     location: Joi.string().max(65),
     position: Joi.string().max(65),
     description: Joi.string().max(1025),
