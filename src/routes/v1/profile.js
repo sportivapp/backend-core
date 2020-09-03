fe8f2c4a77656997dd6d461fa8c04b1db2a75ecf
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-03 21:56:33 +0700

    fix profile route

--- src/routes/v1/profile.js
+++ src/routes/v1/profile.js
@@ -5,7 +5,7 @@ const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
 router.post(routes.profile.company, auth.authenticateToken, profileController.changeUserCompany);
-router.get(routes.profile.company, auth.authenticateToken, profileController.getUserCurrentCompany);
+router.get(routes.profile.currentCompany, auth.authenticateToken, profileController.getUserCurrentCompany);
 router.put(routes.profile.changePassword, auth.authenticateToken, profileController.changePassword);
 router.put(routes.profile.profile, auth.authenticateToken, profileController.updateProfile)
 router.get(routes.profile.profile, auth.authenticateToken, profileController.getProfile)
