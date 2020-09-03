7732b2ada9de1119b4aa5e9a07045e1cfd1f46f4
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-03 21:02:14 +0700

    remove user routes

--- src/routes/v1/profile.js
+++ src/routes/v1/profile.js
@@ -4,10 +4,10 @@ const profileController = require('../../controllers/profile');
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.post( routes.profile.company, auth.authenticateToken, profileController.changeUserCompany);
-router.get( routes.profile.company, auth.authenticateToken, profileController.getUserCurrentCompany);
-router.put( routes.profile.changePassword, auth.authenticateToken, profileController.changePassword);
-router.put( routes.profile.profile, auth.authenticateToken, profileController.updateProfile )
-router.get( routes.profile.profile, auth.authenticateToken, profileController.getProfile )
+router.post(routes.profile.company, auth.authenticateToken, profileController.changeUserCompany);
+router.get(routes.profile.company, auth.authenticateToken, profileController.getUserCurrentCompany);
+router.put(routes.profile.changePassword, auth.authenticateToken, profileController.changePassword);
+router.put(routes.profile.profile, auth.authenticateToken, profileController.updateProfile)
+router.get(routes.profile.profile, auth.authenticateToken, profileController.getProfile)
 
 module.exports = router.expressRouter;
