d665f5c36e8909e359db6a485869ab6278676f54
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-03 20:05:10 +0700

    add new ruotes

--- src/routes/v1/team.js
+++ src/routes/v1/team.js
@@ -4,6 +4,13 @@ const controller = require('../../controllers/team')
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
+router.post( routes.team.list, auth.authenticateToken, controller.createTeam);
+router.post( routes.team.invite, auth.authenticateToken, controller.invite);
+router.post( routes.team.add, auth.authenticateToken, controller.addUserToTeam);
+router.post( routes.team.processInvitation, auth.authenticateToken, controller.processInvitation);
 router.get( routes.team.list, auth.authenticateToken, controller.getTeamsByCompanyId);
+router.get( routes.team.id, auth.authenticateToken, controller.getTeamDetailByCompanyId);
+router.get( routes.team.member, auth.authenticateToken, controller.getTeamMemberList);
+
 
 module.exports = router;
