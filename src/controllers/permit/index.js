58cefa2983fdd3ca5fa37a1d8866a18264219d99
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-03 14:13:02 +0700

    added validation on permit and fix user routes

--- src/controllers/permit/index.js
+++ src/controllers/permit/index.js
@@ -1,10 +1,155 @@
-const Create = require('./Create')
-const List = require('./List')
-const SubordinatePermitList = require('./SubordinatePermitList')
-const RequestApproval = require('./RequestApproval')
-const GetById = require('./GetById')
-const UpdateById = require('./UpdateById')
-const UpdateStatusById = require('./UpdateStatusById')
-const DeleteById = require('./DeleteById')
+const permitService = require('../../services/permitService')
+const ResponseHelper = require('../../helper/ResponseHelper')
 
-module.exports = { Create, List, SubordinatePermitList, RequestApproval, GetById, UpdateById, UpdateStatusById, DeleteById }
+const controller = {}
+
+controller.createPermit = async (req, res, next) => {
+
+    const permitRequest = req.body
+
+    const user = req.user
+
+    if (user.functions.indexOf('C6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    const permitDTO = {
+        eusereuserid: permitRequest.userId,
+        epermitdescription: permitRequest.description,
+        epermitstartdate: permitRequest.startDate,
+        epermitenddate: permitRequest.endDate,
+        epermitcreateby: req.user.sub
+    }
+
+    try {
+        const result = await permitService.createPermit(permitDTO, user)
+        if (!result)
+            return res.status(400).json(ResponseHelper.toErrorResponse(400))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+
+}
+
+controller.getPermitList = async (req, res, next) => {
+
+    const {page, size} = req.query
+
+    const user = req.user
+
+    if (user.functions.indexOf('R6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    try {
+        const pageObj = await permitService.getPermitList(parseInt(page), parseInt(size), user)
+        return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.getPermitById = async (req, res, next) => {
+
+    const {permitId} = req.params
+
+    const user = req.user
+
+    if (user.functions.indexOf('R6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    try {
+        const result = await permitService.getPermitById(permitId)
+        if (!result)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.updatePermitById = async (req, res, next) => {
+
+    const {permitId} = req.params
+
+    const permitRequest = req.body
+
+    const user = req.user
+
+    const permitDTO = {
+        epermitdescription: permitRequest.description,
+        epermitstartdate: permitRequest.startDate,
+        epermitenddate: permitRequest.endDate
+    }
+
+    try {
+        const result = await permitService.updatePermitById(permitId, permitDTO, user)
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.updatePermitStatusById = async (req, res, next) => {
+
+    const {permitId, status} = req.body
+
+    const user = req.user
+
+    if (user.functions.indexOf('U6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    try {
+        const result = await permitService.updatePermitStatusById(permitId, status, user)
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.deletePermitById = async (req, res, next) => {
+
+    const {permitId} = req.params
+
+    const user = req.user
+
+    if (user.functions.indexOf('D6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    try {
+        const result = await permitService.deletePermitById(permitId, user)
+        if (!result)
+            return res.status(400).json(ResponseHelper.toErrorResponse(400))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.requestApproval = async (req, res, next) => {
+
+    const {permitId} = req.params
+
+    const user = req.user
+
+    try {
+        const result = await permitService.requestApproval(parseInt(permitId), user)
+        if (!result)
+            return res.status(404).json(ResponseHelper.toErrorResponse(404))
+        return res.status(200).json(ResponseHelper.toBaseResponse(result))
+    } catch (e) {
+        next(e)
+    }
+}
+
+controller.getSubordinatePermitList = async (req, res, next) => {
+
+    const {page, size} = req.query
+
+    const user = req.user
+
+    if (user.functions.indexOf('R6') === -1) return res.status(403).json(ResponseHelper.toErrorResponse(403))
+
+    try {
+        const pageObj = await permitService.getSubordinatePermitList(parseInt(page), parseInt(size), user)
+        return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging))
+    } catch (e) {
+        next(e)
+    }
+}
+
+module.exports = controller
--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -16,16 +16,12 @@ const { routes } = require('../../constant')
 
 router.post( routes.user.register, auth.authenticateToken, upload.single('employee'), userController.registerEmployees);
 router.post( routes.user.create, auth.authenticateToken, userController.createUser);
-router.post( routes.user.changeCompany, auth.authenticateToken, userController.changeUserCompany);
 router.post( routes.user.login, userController.login);
 router.post( routes.user.forgot, userController.forgotPassword);
 router.get( routes.user.list, auth.authenticateToken, userController.getAllUserByCompanyId);
-router.get( routes.user.currentCompany, auth.authenticateToken, userController.getUserCurrentCompany);
 router.get( routes.user.import, userController.importTemplate);
-router.put( routes.user.password, auth.authenticateToken, userController.changePassword);
 router.get( routes.user.id , auth.authenticateToken, userController.getUserById);
 router.put( routes.user.id , auth.authenticateToken, userController.updateUserById);
 router.delete( routes.user.id , auth.authenticateToken, userController.deleteUserById);
-router.delete( routes.user.approval , auth.authenticateToken, userController.addApprovalUsers);
 
 module.exports = router.expressRouter;
