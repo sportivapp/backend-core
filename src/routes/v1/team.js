a3a6ebec469536bc518f8e79c0424876e4244277
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-04 21:37:14 +0700

    fix the routes from post to get

--- src/routes/v1/team.js
+++ src/routes/v1/team.js
@@ -7,7 +7,7 @@ const { routes } = require('../../constant')
 router.post( routes.team.list, auth.authenticateToken, controller.createTeam);
 router.get( routes.team.list, auth.authenticateToken, controller.getTeams);
 router.get( routes.team.id, auth.authenticateToken, controller.getTeamDetail);
-router.post( routes.team.member, auth.authenticateToken, controller.getTeamMemberList);
+router.get( routes.team.member, auth.authenticateToken, controller.getTeamMemberList);
 router.post( routes.team.invite, auth.authenticateToken, controller.invite);
 router.post( routes.team.processInvitation, auth.authenticateToken, controller.processInvitation);
 router.post( routes.team.processRequest, auth.authenticateToken, controller.processRequest);
