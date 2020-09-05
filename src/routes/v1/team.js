916b323b7b95df44a084181961e8563d5e2ae358
Surya <suryaboyz04@gmail.com>
2020-09-05 11:23:55 +0700

    cleaning

--- src/routes/v1/team.js
+++ src/routes/v1/team.js
@@ -4,15 +4,18 @@ const controller = require('../../controllers/team')
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.post( routes.team.list, auth.authenticateToken, controller.createTeam);
 router.get( routes.team.list, auth.authenticateToken, controller.getTeams);
 router.get( routes.team.id, auth.authenticateToken, controller.getTeamDetail);
+router.post( routes.team.list, auth.authenticateToken, controller.createTeam);
+
+router.post( routes.team.join, auth.authenticateToken, controller.joinTeam);
+
+router.post( routes.team.processRequest, auth.authenticateToken, controller.processRequest);
 router.get( routes.team.member, auth.authenticateToken, controller.getTeamMemberList);
 router.post( routes.team.invite, auth.authenticateToken, controller.invite);
-router.post( routes.team.processInvitation, auth.authenticateToken, controller.processInvitation);
-router.post( routes.team.processRequest, auth.authenticateToken, controller.processRequest);
-router.post( routes.team.join, auth.authenticateToken, controller.joinTeam);
 router.put( routes.team.position, auth.authenticateToken, controller.changeTeamMemberPosition);
 router.delete( routes.team.member, auth.authenticateToken, controller.kick);
+
+router.post( routes.team.processInvitation, auth.authenticateToken, controller.processInvitation);
 
 module.exports = router;
