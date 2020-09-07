eb94bfe528f62f6abd458d1c395b030e2f1d131e
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-07 14:32:23 +0700

    remove unnecessary route

--- src/routes/v1/industry.js
+++ src/routes/v1/industry.js
@@ -4,6 +4,5 @@ const auth = require('../../middlewares/authentication')
 const { routes } = require('../../constant')
 
 router.get(routes.industry.list, controller.getIndustryList);
-router.put(routes.industry.change, auth.authenticateToken, controller.changeIndustryByUserId);
 
 module.exports = router.expressRouter;
