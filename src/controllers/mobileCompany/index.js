cb88954499d6d56c81d52a8361d94b2d7e21b917
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-08 23:02:02 +0700

    add new controller method

--- src/controllers/mobileCompany/index.js
+++ src/controllers/mobileCompany/index.js
@@ -23,14 +23,14 @@ controller.getCompany = async (req, res, next) => {
 
 controller.getCompanies = async (req, res, next) => {
 
-    const { keyword } = req.query
+    const { page, size, keyword } = req.query
 
     try {
-        const result = await companyService.getCompanies(keyword);
+        const pageObj = await companyService.getCompanies(parseInt(page), parseInt(size), keyword);
 
-        if (!result)
+        if (!pageObj)
             return res.status(404).json(ResponseHelper.toErrorResponse(404));
-        return res.status(200).json(ResponseHelper.toBaseResponse(result));
+        return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging));
     } catch(e) {
         next(e);
     }
@@ -62,6 +62,72 @@ controller.joinCompany = async (req, res, next) => {
     try {
 
         const result = await companyService.joinCompany(companyId, req.user);
+
+        return res.status(200).json(ResponseHelper.toBaseResponse(result));
+
+    } catch(e) {
+        next(e);
+    }
+
+}
+
+controller.userCancelJoin = async (req, res, next) => {
+
+    const { companyId } = req.params
+
+    try {
+
+        const result = await companyService.userCancelJoin(parseInt(companyId), req.user.sub);
+
+        return res.status(200).json(ResponseHelper.toBaseResponse(result));
+
+    } catch(e) {
+        next(e);
+    }
+
+}
+
+
+controller.getListPendingInviteByUserId = async (req, res, next) => {
+
+    const {page, size} = req.query
+
+    try {
+
+        const pageObj = await companyService.getListPendingInviteByUserId(parseInt(page), parseInt(size),req.user.sub);
+
+        return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging));
+
+    } catch(e) {
+        next(e);
+    }
+
+}
+
+controller.exitCompany = async (req, res, next) => {
+
+    const { companyId } = req.body;
+    
+    try {
+
+        const result = await companyService.exitCompany(companyId, req.user);
+
+        return res.status(200).json(ResponseHelper.toBaseResponse(result));
+
+    } catch(e) {
+        next(e);
+    }
+
+}
+
+controller.processInvitation = async (req, res, next) => {
+
+    const { companyId } = req.body;
+    const { status } = req.query;
+
+    try {
+
+        const result = await companyService.processInvitation(companyId, req.user, status.toUpperCase());
 
         return res.status(200).json(ResponseHelper.toBaseResponse(result));
 
