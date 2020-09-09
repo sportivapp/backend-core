239ecef13fda2255d678b4c408551e54f1072345
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-09 12:31:49 +0700

    fix migrations

--- src/db/migrations/20200909122249_drop_permit_approval_status.js
+++ src/db/migrations/20200909122249_drop_permit_approval_status.js
@@ -1,7 +1,7 @@
 exports.up = (knex, Promise) => knex.schema.alterTable('epermitapprovalmapping', t => {
-    t.string('epermitapprovalmappingstatus').defaultTo('PENDING').alter()
+    t.dropColumn('epermitapprovalmappingstatus')
 });
 
 exports.down = (knex, Promise) => knex.schema.alterTable('epermitapprovalmapping', t => {
-    t.integer('epermitapprovalmappingstatus').notNullable().defaultTo(1).alter()
+    t.integer('epermitapprovalmappingstatus').notNullable().defaultTo(1)
 });
