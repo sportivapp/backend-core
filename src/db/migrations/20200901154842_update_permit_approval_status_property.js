95a16d6866a050b7452034d8c1f99c2a556db1ac
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-09 12:10:28 +0700

    fix migrations

--- src/db/migrations/20200901154842_update_permit_approval_status_property.js
+++ src/db/migrations/20200901154842_update_permit_approval_status_property.js
@@ -1,5 +1,5 @@
 exports.up = (knex, Promise) => knex.schema.alterTable('epermitapprovalmapping', t => {
-    t.string('epermitapprovalmappingstatus').notNullable().defaultTo('PENDING').alter()
+    t.string('epermitapprovalmappingstatus').defaultTo('PENDING').alter()
 });
 
 exports.down = (knex, Promise) => knex.schema.alterTable('epermitapprovalmapping', t => {
