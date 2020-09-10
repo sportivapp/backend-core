79d5552a33643101ac17cd7d9b11c2f439dd6cd1
Surya <suryaboyz04@gmail.com>
2020-09-11 01:08:53 +0700

    add seed for materi files

--- src/db/seeds/000-file.js
+++ src/db/seeds/000-file.js
@@ -28,5 +28,85 @@ exports.seed = (knex, Promise) => knex('efile').del()
           efilecreateby: 0,
           efilecreatetime: Date.now()
         },
+        {
+          efilename: '10-attack.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/10-attack.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '11-ball feeling A.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/11-ball feeling A.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '12-ball feeling B.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/12-ball feeling B.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '13-heading dan shooting 1.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/13-heading dan shooting 1.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '14-heading dan shooting 2.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/14-heading dan shooting 2.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '15-passing dan control A.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/15-passing dan control A.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '16-passing dan control B.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/16-passing dan control B.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '17-shielding and turning B.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/17-shielding and turning B.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '18-shielding and turning.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/18-shielding and turning.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
+        {
+          efilename: '19-small side games.mp4',
+          efilepath: process.env.TEMP_DIRECTORY + '/19-small side games.mp4',
+          efiletype: 'video/mp4',
+          efilesize: 1,
+          efilecreateby: 0,
+          efilecreatetime: Date.now()
+        },
       ]
 ));
--- src/db/seeds/31-company_file_mapping.js
+++ src/db/seeds/31-company_file_mapping.js
@@ -4,19 +4,83 @@ exports.seed = (knex, Promise) => knex('ecompanyfilemapping').del()
     [
         {
             ecompanyecompanyid: 1,
-            efileefileid: 2,
+            efileefileid: 4,
             ecompanyfilemappingcreatetime: Date.now(),
-            ecompanyfilemappingcreateby: 1,
+            ecompanyfilemappingcreateby: 0,
             ecompanyfilemappingchangetime: Date.now(),
-            ecompanyfilemappingchangeby: 1
+            ecompanyfilemappingchangeby: 0
         },
         {
           ecompanyecompanyid: 1,
-          efileefileid: 3,
+          efileefileid: 5,
           ecompanyfilemappingcreatetime: Date.now(),
-          ecompanyfilemappingcreateby: 1,
+          ecompanyfilemappingcreateby: 0,
           ecompanyfilemappingchangetime: Date.now(),
-          ecompanyfilemappingchangeby: 1
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 6,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 7,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 8,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 9,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 10,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 11,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 12,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
+        },
+        {
+          ecompanyecompanyid: 1,
+          efileefileid: 13,
+          ecompanyfilemappingcreatetime: Date.now(),
+          ecompanyfilemappingcreateby: 0,
+          ecompanyfilemappingchangetime: Date.now(),
+          ecompanyfilemappingchangeby: 0
         },
     ]
   ));
