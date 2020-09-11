f09d6fb1308470c1098bc83d85a42962eab11424
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-11 20:33:30 +0700

    remove with graph fetched

--- src/models/Class.js
+++ src/models/Class.js
@@ -37,7 +37,6 @@ class Class extends Model {
                     'eclasspicname',
                     'eclasspicmobilenumber'
                 )
-                .withGraphFetched('picture')
             }
         }
     }
