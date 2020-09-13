404bf4a2e02a877edbb94cfad747cfebd2ac23c0
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-13 13:47:10 +0700

    remove unnecessary company Id data process

--- src/services/mobileNews.js
+++ src/services/mobileNews.js
@@ -3,30 +3,20 @@ const Company = require('../models/Company')
 const { UnsupportedOperationError, NotFoundError } = require('../models/errors')
 
 const UnsupportedOperationErrorEnum = {
-    NEWS_IN_COMPANY_NOT_EXIST: 'NEWS_IN_COMPANY_NOT_EXIST',
-    COMPANY_NOT_EXIST: 'COMPANY_NOT_EXIST'
+    NEWS_NOT_EXIST: 'NEWS_NOT_EXIST',
 }
 
 const mobileNewsService = {}
 
-mobileNewsService.getNewsDetail = async (companyId, newsId) => {
-
-    const company = await Company.query()
-    .select()
-    .where('ecompanyid', companyId)
-    .first()
-
-    if(!company)
-        throw new UnsupportedOperationError(UnsupportedOperationErrorEnum.COMPANY_NOT_EXIST)
+mobileNewsService.getNewsDetail = async (newsId) => {
 
     const newsFromDB = await News.query()
     .select()
     .where('enewsid', newsId)
-    .where('ecompanyecompanyid', companyId)
     .first()
 
     if(!newsFromDB)
-        throw new UnsupportedOperationError(UnsupportedOperationErrorEnum.NEWS_IN_COMPANY_NOT_EXIST)
+        throw new UnsupportedOperationError(UnsupportedOperationErrorEnum.NEWS_NOT_EXIST)
 
     return newsFromDB
 }
