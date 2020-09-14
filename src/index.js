42cda8773c2ed679cd22ee90c89faeacb29e4eb6
Surya <suryaboyz04@gmail.com>
2020-09-14 12:28:54 +0700

    comment https config

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ const   express     = require('express'),
 require('dotenv').config();
 const app = express();
 const routes = require('./routes/v1');
-const slackLoggingService = require('./helper/slackLoggingService');
 const errorHandler = require('./middlewares/errorHandler');
-
-const webHookURL = 'https://hooks.slack.com/services/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';
-let errorMsg = {};
 
 app.use(cors());
 app.use((_, res, next) => {
@@ -65,17 +61,17 @@ const httpServer = app.listen(httpPORT, function() {
 })
 
 // configuration for https
- const options = {
-    key: fs.readFileSync('../../../etc/ssl/private/quickplay.key', 'utf8'),
-    cert: fs.readFileSync('../../../etc/ssl/certs/quickplay.crt', 'utf8')
-};
-const httpsServer = https.createServer(options, app);
-const httpsPORT = process.env.HTTPS_PORT || 5101;
-httpsServer.listen(httpsPORT, function() {
-    console.log(`HTTPS Server started on port ${httpsPORT}.`);
-});
+//  const options = {
+//     key: fs.readFileSync('../../../etc/ssl/private/quickplay.key', 'utf8'),
+//     cert: fs.readFileSync('../../../etc/ssl/certs/quickplay.crt', 'utf8')
+// };
+// const httpsServer = https.createServer(options, app);
+// const httpsPORT = process.env.HTTPS_PORT || 5101;
+// httpsServer.listen(httpsPORT, function() {
+//     console.log(`HTTPS Server started on port ${httpsPORT}.`);
+// });
 
 module.exports = {
     httpServer: httpServer,
-    httpsServer: httpsServer
+    // httpsServer: httpsServer
 }
