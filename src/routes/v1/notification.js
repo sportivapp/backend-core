2a30041a9d4e54ef437dfd79f97064ed3c1dbbe6
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-17 22:55:23 +0700

    remove unnecessary route

--- src/routes/v1/notification.js
+++ src/routes/v1/notification.js
@@ -5,7 +5,6 @@ const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant');
 
 router.get( routes.notification.list, auth.authenticateToken, controller.getAllNotification);
-router.get( routes.notification.body, auth.authenticateToken, controller.getAllNotificationBody);
-router.delete( routes.notification.id, auth.authenticateToken, controller.deleteNotification);
+router.delete( routes.notification.list, auth.authenticateToken, controller.deleteNotificationBody);
 
 module.exports = router;
