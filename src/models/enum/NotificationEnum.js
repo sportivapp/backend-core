af6ec9c25db2ed060268c9500fbdcb4aba4c7070
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-18 13:38:50 +0700

    initial commit

---
+++ src/models/enum/NotificationEnum.js
@@ -0,0 +1,17 @@
+const NotificationEnum = {
+    company: {
+        type: 'COMPANY',
+        actions: {
+
+        }
+    },
+    class: {
+        type: 'CLASS',
+        actions: {
+
+        }
+
+    }
+}
+
+module.exports = NotificationEnum
