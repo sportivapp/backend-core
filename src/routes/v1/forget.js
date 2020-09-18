568492cd34267ffe8eaf3062207274e739e2e6a7
Surya <suryaboyz04@gmail.com>
2020-09-18 17:36:56 +0700

    token and email as params on set password

--- src/routes/v1/forget.js
+++ src/routes/v1/forget.js
@@ -3,6 +3,6 @@ const controller = require('../../controllers/forget');
 
 router.post('/forget-email', controller.sendForgetEmail);
 router.get('/forget-check/:token/:email', controller.checkForgetLink);
-router.post('/forget-set-password', controller.setPassword);
+router.post('/forget-set-password/:token/:email', controller.setPassword);
 
 module.exports = router.expressRouter;
