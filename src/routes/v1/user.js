1e28c8fcd9b00a6881ad04cf39d9862945f1d49f
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-21 13:40:14 +0700

    add new user profile route

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -21,6 +21,7 @@ router.post( routes.user.forgot, userController.forgotPassword);
 router.get( routes.user.list, auth.authenticateToken, userController.getAllUserByCompanyId);
 router.get( routes.user.import, userController.importTemplate);
 router.get( routes.user.id , auth.authenticateToken, userController.getUserById);
+router.post( routes.user.profile, auth.authenticateToken, userController.getOtherUserById);
 router.put( routes.user.id , auth.authenticateToken, userController.updateUserById);
 router.delete( routes.user.id , auth.authenticateToken, userController.deleteUserById);
 router.delete( routes.user.approval , auth.authenticateToken, userController.addApprovalUsers);
