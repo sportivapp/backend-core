27b2ff451453205de6d13afed8b8ba56d659c08b
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-24 10:00:44 +0700

    delete remove permission column in migration

--- src/db/migrations/20200831145034_remove_permission_company_user_mapping.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex, Promise) => knex.schema.alterTable('ecompanyusermapping', t => {
-    t.dropColumn('ecompanyusermappingpermission');
-});
-
-exports.down = (knex, Promise) => knex.schema.alterTable('ecompanyusermapping', t => {
-    t.integer('ecompanyusermappingpermission').notNullable().defaultTo(1);
-});
