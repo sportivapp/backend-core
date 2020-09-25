68951cef51ee323870f2ca081c4a681e0f736387
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-09-25 14:49:37 +0700

    change route to post

--- src/routes/v1/experience.js
+++ src/routes/v1/experience.js
@@ -1,8 +1,9 @@
-const router = require('../router')
+const express = require('express');
+const router = express.Router();
 const controller = require('../../controllers/experience');
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.get(routes.experience.id, auth.authenticateToken, controller.getExperienceById);
+router.post(routes.experience.id, auth.authenticateToken, controller.getExperienceById);
 
-module.exports = router.expressRouter;
+module.exports = router;
--- src/routes/v1/license.js
+++ src/routes/v1/license.js
@@ -4,6 +4,6 @@ const controller = require('../../controllers/license')
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.get( routes.license.id, auth.authenticateToken, controller.getLicenseById);
+router.post( routes.license.id, auth.authenticateToken, controller.getLicenseById);
 
 module.exports = router;
