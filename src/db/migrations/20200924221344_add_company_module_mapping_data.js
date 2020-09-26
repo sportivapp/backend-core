75155f52463049b737eaf3b8a0dfae91ac1fec98
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-09-26 16:03:27 +0700

    fix accessing unknown object and fix auto increment data

--- src/db/migrations/20200924221344_add_company_module_mapping_data.js
+++ src/db/migrations/20200924221344_add_company_module_mapping_data.js
@@ -1,36 +1,32 @@
 exports.up = (knex) =>
     knex.transaction(trx => {
-        knex('ecompany')
-            .select('ecompanyid')
-            .whereNull('ecompanyparentid')
+        knex('ecompanymodulemapping')
+            .del()
+            .then(ignored => {
+                return knex('ecompany')
+                    .select('ecompanyid')
+                    .whereNull('ecompanyparentid')
+            })
             .then(companies => {
                 return knex('emodule')
                     .select('emoduleid', 'emodulename')
                     .then(modules => ({ companies: companies, modules: modules }))
             })
-            .then(({ companies, modules }) => {
-                return knex('ecompanymodulemapping')
-                    .orderBy('ecompanymodulemappingid', 'DESC')
-                    .first()
-                    .then(mapping => ({ companies: companies, modules: modules, startId: mapping.ecompanymodulemappingid }))
-            })
-            .then(({ companies, modules, startId }) => createCompanyModuleMappings(companies, modules, startId, knex, trx))
+            .then(({ companies, modules }) => createCompanyModuleMappings(companies, modules, knex, trx))
             .then(trx.commit)
             .catch(trx.rollback)
     })
 
-function createCompanyModuleMappings(companies, modules, startId, knex, trx) {
+function createCompanyModuleMappings(companies, modules, knex, trx) {
     let companyModules = []
     companies.forEach(company => {
         modules.forEach(module => {
             const companyModule = {
-                ecompanymodulemappingid: startId + 1,
                 ecompanyecompanyid: company.ecompanyid,
                 emoduleemoduleid: module.emoduleid,
                 ecompanymodulemappingname: module.emodulename
             }
             companyModules.push(companyModule)
-            startId++
         })
     })
     return knex('ecompanymodulemapping').insert(companyModules)
