0a5a83d4759aee0433b6a3db3722d740a9ee002f
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-10-02 14:39:11 +0700

    add routes

--- src/routes/v1/theory.js
+++ src/routes/v1/theory.js
@@ -1,7 +1,24 @@
 const router = require('../router')
-const theoryController = require('../../controllers/theory');
-const auth = require('../../middlewares/authentication');
+const theoryController = require('../../controllers/theory')
+const auth = require('../../middlewares/authentication')
 
-router.get('/theory', auth.authenticateToken, theoryController.getTheoryList);
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const temp = `${process.env.TEMP_DIRECTORY}`
+        cb(null, temp);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+const upload = multer({storage: storage})
+const { routes } = require('../../constant')
+
+router.get( routes.theory.list, auth.authenticateToken, theoryController.getTheoryList)
+router.post( routes.theory.list, auth.authenticateToken, upload.single('file'), theoryController.createTheory)
+router.post( routes.theory.download, auth.authenticateToken, theoryController.downloadTheory);
+router.post( routes.theory.preview, auth.authenticateToken, theoryController.previewTheory);
+router.post( routes.theory.remove, auth.authenticateToken, theoryController.deleteTheoryByFileId)
 
 module.exports = router.expressRouter;
