9d03931081f0d92739825423c7a2ea8669376348
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2020-10-05 12:13:30 +0700

    fix migration file

--- src/db/migrations/20201005110453_thread_post.js
+++ src/db/migrations/20201005110453_thread_post.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => knex.schema.createTable('ethreadpost', t => {
     t.increments('ethreadpostid').primary().unsigned();
     t.string('ethreadpostcomment', 500).notNullable();
-    t.string('ethreadethreadid').notNullable().references('ethread.ethreadid').onDelete('CASCADE');
-    t.string('efileefileid').references('efile.efileid').onDelete('SET NULL');
+    t.integer('ethreadethreadid').notNullable().references('ethread.ethreadid').onDelete('CASCADE');
+    t.integer('efileefileid').references('efile.efileid').onDelete('SET NULL');
     t.bigInteger('ethreadpostcreatetime').notNullable();
     t.integer('ethreadpostcreateby').notNullable();
     t.bigInteger('ethreadpostchangetime');
