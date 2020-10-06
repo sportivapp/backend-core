a14257400b0a680b6c1d5c4f7c2c5d7ca86b5d3b
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-10-06 16:49:48 +0700

    add validation to return data

--- src/services/mobileForumService.js
+++ src/services/mobileForumService.js
@@ -5,15 +5,45 @@ const TimeEnum = require('../models/enum/TimeEnum')
 
 const mobileForumService = {}
 
-mobileForumService.getThreadList = async (page, size) => {
+mobileForumService.getThreadList = async (page, size, filter) => {
 
+    if(filter === undefined)
+        return Thread.query()
+        .modify('baseAttributes')
+        .where('ethreadcreatetime', '>', Date.now() - TimeEnum.THREE_MONTHS)
+        .orderBy('ethreadcreatetime', 'DESC')
+        .page(page, size)
+        .then(pageObj => ServiceHelper.toPageObj(page, size, pageObj))
+
+    if(filter.companyId === undefined)
+        return Thread.query()
+        .modify('baseAttributes')
+        .where('eteameteamid', filter.teamId)
+        .where('ethreadcreatetime', '>', Date.now() - TimeEnum.THREE_MONTHS)
+        .orderBy('ethreadcreatetime', 'DESC')
+        .page(page, size)
+        .then(pageObj => ServiceHelper.toPageObj(page, size, pageObj))
+
+    if(filter.teamId === undefined)
+        return Thread.query()
+        .modify('baseAttributes')
+        .where('ecompanyecompanyid', filter.companyId)
+        .where('ethreadcreatetime', '>', Date.now() - TimeEnum.THREE_MONTHS)
+        .orderBy('ethreadcreatetime', 'DESC')
+        .page(page, size)
+        .then(pageObj => ServiceHelper.toPageObj(page, size, pageObj))
+
+    // if filter isn't undefined and teamid and companyid isn't undefined too, then do this
     return Thread.query()
     .modify('baseAttributes')
+    .where('eteameteamid', filter.teamId)
+    .where('ecompanyecompanyid', filter.companyId)
     .where('ethreadcreatetime', '>', Date.now() - TimeEnum.THREE_MONTHS)
     .orderBy('ethreadcreatetime', 'DESC')
     .page(page, size)
     .then(pageObj => ServiceHelper.toPageObj(page, size, pageObj))
 
+    
 }
 
 mobileForumService.getThreadDetailById = async (threadId) => {
