139b76099c42bef2d02d9ecc3928330475aa0d26
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-10-12 15:58:04 +0700

    add delete, and change getteammemberlist route

--- src/routes/v1/team.js
+++ src/routes/v1/team.js
@@ -11,7 +11,7 @@ router.post( routes.team.join, auth.authenticateToken, controller.joinTeam);
 router.post( routes.team.exit, auth.authenticateToken, controller.exitTeam)
 router.post( routes.team.cancelInvitation, auth.authenticateToken, controller.cancelInvite);
 router.post( routes.team.processRequest, auth.authenticateToken, controller.processRequest);
-router.post( routes.team.member, auth.authenticateToken, controller.getTeamMemberList); //TODO: Change route later like the one below?
+router.get( routes.team.members, auth.authenticateToken, controller.getTeamMemberList);
 router.get( routes.team.memberByLogType, auth.authenticateToken, controller.getTeamMemberByLogType);
 router.post( routes.team.invite, auth.authenticateToken, controller.invite);
 router.put( routes.team.position, auth.authenticateToken, controller.changeTeamMemberPosition);
@@ -20,5 +20,6 @@ router.post( routes.team.kick, auth.authenticateToken, controller.kickUserFromTe
 router.post( routes.team.cancelRequest, auth.authenticateToken, controller.cancelRequest)
 router.post( routes.team.processInvitation, auth.authenticateToken, controller.processInvitation);
 router.get(routes.team.invite, auth.authenticateToken, controller.getMembersToInvite);
+router.delete(routes.team.id, auth.authenticateToken, controller.deleteTeam);
 
 module.exports = router;
