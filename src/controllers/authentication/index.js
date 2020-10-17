463c034a1ce9b2e08aa458cd0b2147b169d94c94
Surya <suryaboyz04@gmail.com>
2020-10-17 12:24:20 +0700

    login company returns redirected url and set cookie

--- src/controllers/authentication/index.js
+++ src/controllers/authentication/index.js
@@ -32,14 +32,14 @@ controller.loginCompany = async (req, res, next) => {
 
         const result = await authenticationService.loginCompany(companyId, req.user);
 
-        // return res.status(200).json(ResponseHelper.toBaseResponse(result));
         res.cookie('tok', result, {
             secure: true,
             httpOnly: true,
             domain: process.env.COOKIE_DOMAIN,
             maxAge: 15 * 60 * 1000
         })
-        return res.redirect(302, process.env.ORG_DOMAIN);
+        // return res.redirect(302, process.env.ORG_DOMAIN);
+        return res.status(200).json(ResponseHelper.toBaseResponse(process.env.ORG_DOMAIN));
 
     } catch(e) {
         next(e);
