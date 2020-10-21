6e8346baf8f0cd1ff920dd8195393b122e9af400
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2020-10-21 13:38:38 +0700

    add new route

--- src/routes/v1/companyLog.js
+++ src/routes/v1/companyLog.js
@@ -5,6 +5,7 @@ const { routes } = require('../../constant');
 
 router.post(routes.companyLog.list, auth.authenticateToken, companyLogController.inviteMember)
 router.get(routes.companyLog.list, auth.authenticateToken, companyLogController.getLogList)
+router.get(routes.companyLog.listPending, auth.authenticateToken, companyLogController.getUserCompanyPendingListByLogType)
 router.put(routes.companyLog.processRequest, auth.authenticateToken, companyLogController.processRequests)
 router.delete(routes.companyLog.cancelInvite, auth.authenticateToken, companyLogController.cancelInvites)
 
