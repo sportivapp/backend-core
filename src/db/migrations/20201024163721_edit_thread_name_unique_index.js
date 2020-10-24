2702cf77a39781a8c4e725b95253714c4cebe943
Oliver Sebastian <oliverrsebastian@gmail.com>
2020-10-24 16:41:12 +0700

    edit unique index to support soft delete

---
+++ src/db/migrations/20201024163721_edit_thread_name_unique_index.js
@@ -0,0 +1,15 @@
+exports.up = (knex, Promise) => deleteIndex(knex)
+    .then(() => knex.schema.raw(
+        `CREATE UNIQUE INDEX "forum_name_companyId_unique" ON "ethread" (lower("ethreadtitle"), "ecompanyecompanyid", "ethreaddeletestatus") WHERE "eteameteamid" IS NULL AND "ecompanyecompanyid" IS NOT NULL`
+    ))
+    .then(() => knex.schema.raw(
+        `CREATE UNIQUE INDEX "forum_name_teamId_unique" ON "ethread" (lower("ethreadtitle"), "eteameteamid", "ethreaddeletestatus") WHERE "ecompanyecompanyid" IS NULL AND "eteameteamid" IS NOT NULL`
+    )).then(() => knex.schema.raw(
+        `CREATE UNIQUE INDEX "forum_name_userId_unique" ON "ethread" (lower("ethreadtitle"), "ethreaddeletestatus") WHERE "ecompanyecompanyid" IS NULL AND "eteameteamid" IS NULL`
+    ))
+
+function deleteIndex(knex) {
+    knex.schema.raw('DROP INDEX "forum_name_userId_unique", "forum_name_teamId_unique", "forum_name_companyId_unique"')
+}
+
+exports.down = (knex, Promise) => deleteIndex(knex);
