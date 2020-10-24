3b6758b9def2fbe24b1460cd975ee682458ec4a2
Surya <suryaboyz04@gmail.com>
2020-10-24 15:59:35 +0700

    add typeId

--- src/controllers/reportThread/index.js
+++ src/controllers/reportThread/index.js
@@ -7,9 +7,9 @@ reportThreadController.createReportThread = async (req, res, next) => {
 
     try {
 
-        const { message, threadId, commentId, replyId } = req.body;
+        const { message, threadId, commentId, replyId, typeId } = req.body;
 
-        const result = await reportThreadService.report(message, threadId, commentId, replyId, req.user);
+        const result = await reportThreadService.report(message, threadId, commentId, replyId, typeId, req.user);
         return res.status(200).json(ResponseHelper.toBaseResponse(result));
 
     } catch(e) {
