921bab943f2813e1d4e132c6f23c56d0170c2ed8
Oliver Sebastian <oliverrsebastian@gmail.com>
2020-11-05 20:07:46 +0700

    fix today query param default value

--- src/controllers/newsUser/index.js
+++ src/controllers/newsUser/index.js
@@ -19,13 +19,13 @@ controller.generateNewsLink = async (req, res, next) => {
 
 controller.getNews = async (req, res, next) => {
 
-    const { page = '0', size = '10', companyId = null, category = null, today = false } = req.query
+    const { page = '0', size = '10', companyId = null, category = null, today = 'false' } = req.query
 
     const pageRequest = { page: parseInt(page), size: parseInt(size) }
 
     try {
 
-        const pageObj = await newsUserService.getNews(pageRequest, req.user, companyId, category, today)
+        const pageObj = await newsUserService.getNews(pageRequest, req.user, companyId, category, today === 'true')
         return res.status(200).json(ResponseHelper.toPageResponse(pageObj.data, pageObj.paging))
         
     } catch (e) {
