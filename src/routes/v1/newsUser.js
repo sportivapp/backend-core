d01e91567e28eeda27e3a24abb70fc7a1b8eb09c
Oliver Sebastian <oliverrsebastian@gmail.com>
2020-11-10 14:48:28 +0700

    open user news API for guests

--- src/routes/v1/newsUser.js
+++ src/routes/v1/newsUser.js
@@ -3,11 +3,11 @@ const controller = require('../../controllers/newsUser')
 const auth = require('../../middlewares/authentication');
 const { routes } = require('../../constant')
 
-router.post( routes.newsUser.like, auth.authenticateToken, controller.likeNews);
-router.get( routes.newsUser.generate, auth.authenticateToken, controller.generateNewsLink);
-router.get( routes.newsUser.list, auth.authenticateToken, controller.getNews);
-router.get( routes.newsUser.id, auth.authenticateToken, controller.getNewsDetail);
-router.get( routes.newsUser.count, auth.authenticateToken, controller.getUserViewCount);
-router.delete( routes.newsUser.like, auth.authenticateToken, controller.removeLikeNews);
+router.get( routes.newsUser.like, auth.authenticateTokenIfExist, controller.likeNews);
+router.get( routes.newsUser.generate, auth.authenticateTokenIfExist, controller.generateNewsLink);
+router.get( routes.newsUser.list, auth.authenticateTokenIfExist, controller.getNews);
+router.get( routes.newsUser.id, auth.authenticateTokenIfExist, controller.getNewsDetail);
+router.get( routes.newsUser.count, auth.authenticateTokenIfExist, controller.getUserViewCount);
+router.delete( routes.newsUser.like, auth.authenticateTokenIfExist, controller.removeLikeNews);
 
 module.exports = router.expressRouter;
