02cc1fa5ae6fde04d259ddb258ca4882a13f524e
Oliver Sebastian <oliverrsebastian@gmail.com>
2020-11-12 18:54:57 +0700

    fix bugs

--- src/webmodel/request/news/CreateNewsRequest.js
+++ src/webmodel/request/news/CreateNewsRequest.js
@@ -1,10 +1,10 @@
 const Joi = require('joi')
 
 const schema = Joi.object({
-    title: Joi.string().max(255),
-    content: Joi.string().min(1),
+    title: Joi.string().max(255).allow(null, ''),
+    content: Joi.string().allow(null, ''),
     fileId: Joi.number().positive().allow(null),
-    industryId: Joi.number().positive(),
+    industryId: Joi.number().positive().allow(null),
     isPublic: Joi.boolean().required()
 })
 
