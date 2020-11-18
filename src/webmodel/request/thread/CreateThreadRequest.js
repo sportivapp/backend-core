f4778ddd441bb8996b9fa8d2e53400ec223e9801
Surya <suryaboyz04@gmail.com>
2020-11-18 20:53:18 +0700

    more fixes

--- src/webmodel/request/thread/CreateThreadRequest.js
+++ src/webmodel/request/thread/CreateThreadRequest.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const schema = Joi.object({
     title: Joi.string().required(),
-    description: Joi.string().max(1025).required(),
+    description: Joi.string().min(1).max(1025).required(),
     isPublic: Joi.boolean().required(),
     companyId: Joi.number().allow(null),
     teamId: Joi.number().allow(null),
