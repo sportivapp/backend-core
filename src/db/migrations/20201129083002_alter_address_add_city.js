b97c17fcda2094c6f30bd9558c18707e90d9b387
Surya <suryaboyz04@gmail.com>
2020-11-30 13:25:38 +0700

    alter

--- src/db/migrations/20201129083002_alter_address_add_city.js
+++ src/db/migrations/20201129083002_alter_address_add_city.js
@@ -1,5 +1,6 @@
 exports.up = (knex, Promise) => knex.schema.alterTable('eaddress', t => {
     t.integer('ecityecityid').references('ecity.ecityid');
+    t.integer('estateestateid').nullable().alter();
 });
 
 exports.down = (knex, Promise) => knex.schema.alterTable('eaddress', t => {
