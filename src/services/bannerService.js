c7bbae67a8ae601dc19167fbb5ab306baad34ecc
Surya <suryaboyz04@gmail.com>
2021-01-13 01:15:41 +0700

    add validation for insertBanner

--- src/services/bannerService.js
+++ src/services/bannerService.js
@@ -1,9 +1,21 @@
 const Banner = require('../models/Banner');
 const BannerStatusEnum = require('../models/enum/BannerStatusEnum');
+const { UnsupportedOperationError } = require('../models/errors');
+const fileService = require('./fileService');
+
+const ErrorEnum = {
+    INVALID_FILE_OWNER: 'INVALID_FILE_OWNER',
+}
 
 bannerService = {};
 
 bannerService.insertBanner = async (bannerDTO, user) => {
+
+    await fileService.getFileByIdAndCreateBy(bannerDTO.efileefileid, user.sub)
+        .then(file => {
+            if (!file)
+                throw UnsupportedOperationError(ErrorEnum.INVALID_FILE_OWNER);
+        });
 
     return Banner.query()
         .insertToTable(bannerDTO, user.sub);
