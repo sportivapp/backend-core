872abd30b90c1ede55bc794604adaa1833eae36f
Surya <suryaboyz04@gmail.com>
2021-01-19 20:03:04 +0700

    remove unused code

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -200,18 +200,4 @@ userController.login = async (req, res, next) => {
 
 }
 
-userController.importTemplate = async (req, res, next) => {
-
-    try {
-
-        res.setHeader('Content-disposition', 'attachment; filename=Import Data Karyawan Template.xlsx');
-        res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        return res.download(templatePath);
-
-    } catch(e) {
-        next(e);
-    }
-
-}
-
 module.exports = userController
