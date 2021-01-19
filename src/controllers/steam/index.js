604bd448411cbb214a20a96b7e7dfcf0b831bdfb
Surya <suryaboyz04@gmail.com>
2021-01-19 19:28:13 +0700

    remove console log, use env

--- src/controllers/steam/index.js
+++ src/controllers/steam/index.js
@@ -7,11 +7,9 @@ steamController.redirect = async(req, res, next) => {
     try {
 
         const redirectUrl = await steam.getRedirectUrl();
-        console.log(redirectUrl);
         return res.redirect(redirectUrl);
 
     } catch(e){
-        console.log(e);
         next(e);
     }
 
@@ -22,10 +20,8 @@ steamController.authenticate = async(req, res, next) => {
     try {
 
         const user = await steam.authenticate(req);
-        console.log(user);
 
     } catch(e){
-        console.log(e);
         next(e);
     }
 
--- src/helper/steamAuthService.js
+++ src/helper/steamAuthService.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const SteamAuth = require("node-steam-openid");
  
 const steam = new SteamAuth({
-  realm: "http://api.quickplay.app", // Site name displayed to users on logon
-  returnUrl: "http://api.quickplay.app/auth/steam/authenticate", // Your return route
-  apiKey: "01CEB147BCC53C5A227591F42D1065DF" // Steam API key
+  realm: process.env.STEAM_REALM, // Site name displayed to users on logon
+  returnUrl: process.env.STEAM_RETURN_URL, // Your return route
+  apiKey: process.env.STEAM_API_KEY // Steam API key
 });
 
 module.exports = steam;
