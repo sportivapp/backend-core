23e7774d918f1c184b12b7b10ebb915c78150bf2
Surya <suryaboyz04@gmail.com>
2021-01-22 18:42:02 +0700

    use routes

--- src/routes/v1/mobile/mobileUser.js
+++ src/routes/v1/mobile/mobileUser.js
@@ -4,16 +4,16 @@ const auth = require('../../../middlewares/authentication');
 const { routes } = require('../../../constant');
 const { getIndustryByUserId } = require('../../../services/mobileUserService');
 
-router.post('/user', controller.createUser);
-router.post('/user-login', controller.login);
+router.post( routes.user.list, controller.createUser);
+router.post( routes.user.login, controller.login);
 router.put( routes.user.coach, auth.authenticateToken, controller.updateUserCoachData);
 router.delete( routes.user.removeCoach, auth.authenticateToken, controller.removeCoach);
-router.post('/user-profile', auth.authenticateToken, controller.getOtherUserById);
-router.get('/user-self-profile', auth.authenticateToken, controller.getSelf);
+router.post( routes.user.profile, auth.authenticateToken, controller.getOtherUserById);
+router.get( routes.user.selfProfile, auth.authenticateToken, controller.getSelf);
 router.get( routes.user.pending, auth.authenticateToken, controller.getListPendingByUserId);
-router.put('/user', auth.authenticateToken, controller.updateUser);
+router.put( routes.user.list, auth.authenticateToken, controller.updateUser);
 router.get( routes.user.industry, auth.authenticateToken, controller.getIndustryByUserId );
 router.put( routes.user.industry, auth.authenticateToken, controller.changeIndustryByUserId);
-router.post('/user-change-password', auth.authenticateToken, controller.changePassword);
+router.post( routes.user.changePassword, auth.authenticateToken, controller.changePassword);
 
 module.exports = router.expressRouter;
