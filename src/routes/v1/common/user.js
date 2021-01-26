5001b8030758083d95acd02b65a71f735f494763
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-01-26 16:03:30 +0700

    add new get route

--- src/routes/v1/common/user.js
+++ src/routes/v1/common/user.js
@@ -12,5 +12,6 @@ router.get( routes.user.id , auth.authenticateToken, userController.getUserById)
 router.post( routes.user.profile, auth.authenticateToken, userController.getOtherUserById);
 router.put( routes.user.id , auth.authenticateToken, userController.updateUserById);
 router.delete( routes.user.id , auth.authenticateToken, userController.deleteUserById);
+router.get( routes.user.searchByName , auth.authenticateToken, userController.getUsersByName);
 
 module.exports = router.expressRouter;
