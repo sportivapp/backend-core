45ed23a0dbf133a55ed02d079e382c86ad733f69
Surya <suryaboyz04@gmail.com>
2021-02-02 16:30:52 +0700

    remove console log

--- src/controllers/notification/index.js
+++ src/controllers/notification/index.js
@@ -26,7 +26,6 @@ controller.readNotification = async (req, res, next) => {
         return res.status(200).json(ResponseHelper.toBaseResponse(result));
 
     } catch(e) {
-        console.log(e);
         next(e);
     }
 
