b8449de6f5969836976aa07e6f6566756a4737ba
Surya <suryaboyz04@gmail.com>
2021-02-10 01:02:36 +0700

    feat(notification): notify reply to reply

---
+++ src/db/migrations/20210210005131_alter_thread_post_reply.js
@@ -0,0 +1,7 @@
+exports.up = (knex, Promise) => knex.schema.alterTable('ethreadpostreply', t => {
+    t.integer('ethreadpostreplyethreadpostreplyid');
+});
+
+exports.down = (knex, Promise) => knex.schema.alterTable('ethreadpostreply', t => {
+    t.dropColumn('enotificationisclicked');
+});
