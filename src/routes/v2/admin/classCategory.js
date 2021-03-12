f8d0f1646ae5a8df57d4058a91c100ed0dad7146
Surya <suryacoolboyz@hotmail.com>
2021-03-12 13:11:15 +0700

    feat(class): add route for get schedule (#49)

--- src/routes/v2/admin/classCategory.js
+++ src/routes/v2/admin/classCategory.js
@@ -4,5 +4,6 @@ const { routes } = require('../../../constant');
 const controller = require('../../../controllers/v2/classCategory');
 
 router.post(routes.classCategory.extend, auth.authenticateToken, controller.extend);
+router.get(routes.classCategory.schedule, auth.authenticateToken, controller.getSchedules);
 
 module.exports = router.expressRouter;
