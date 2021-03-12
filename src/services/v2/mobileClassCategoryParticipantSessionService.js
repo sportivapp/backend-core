2f53cc0b2984dc3939ac93f1540f10f3d9fa9faf
Staticerz <suryacoolboyz@hotmail.com>
2021-03-12 13:24:48 +0700

    feat(class): add logic on class confirmation
    only appears after end session, not showing after two days of end session

--- src/services/v2/mobileClassCategoryParticipantSessionService.js
+++ src/services/v2/mobileClassCategoryParticipantSessionService.js
@@ -89,10 +89,10 @@ classCategoryParticipantSessionService.updateParticipantConfirmedExpiration = as
 
     const twoDays = timeService.getTimeInMilis(1, 1, 2, 1, 1, 1);
 
-    return ClassCategoryParticipantSession.query(trx)
+    return ClassCategoryParticipantSession.query()
         .where('class_category_session_uuid', classCategorySessionUuid)
         .patch({
-            confirmedExpiration = Date.now() + twoDays,
+            confirmedExpiration: Date.now() + twoDays,
         });
 
 }
