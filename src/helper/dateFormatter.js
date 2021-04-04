b7bed4cc6d7a229340477f981712ab56b82cf91a
Staticerz <suryacoolboyz@hotmail.com>
2021-04-05 00:43:38 +0700

    feat(complaint id): add complaint id on complaint

---
+++ src/helper/dateFormatter.js
@@ -0,0 +1,9 @@
+exports.formatDateToYYYYMMDD = (date) => {
+
+    var day = ('0' + date.getDate()).slice(-2);
+    var month = ('0' + (date.getMonth() + 1)).slice(-2);
+    var year = date.getFullYear();
+
+    return year + month + day;
+
+}
