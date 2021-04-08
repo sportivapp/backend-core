027bf6fe28ff6122be573e364a3b05d8e1c49270
Staticerz <suryacoolboyz@hotmail.com>
2021-04-09 00:06:49 +0700

    fix(migration): return Promise on sequence migration

--- src/db/migrations/20210317013615_class_transaction_sequence.js
+++ src/db/migrations/20210317013615_class_transaction_sequence.js
@@ -1,9 +1,9 @@
 const sequenceName = 'class_transaction_seq';
 
-exports.up = async (knex) => {
+exports.up = async (knex, Promise) => {
     knex.raw(`CREATE SEQUENCE ${sequenceName} START 1`);
 };
 
-exports.down = function(knex) {
+exports.down = function(knex, Promise) {
     knex.raw(`DROP SEQUENCE IF EXISTS ${sequenceName}`);
 };
