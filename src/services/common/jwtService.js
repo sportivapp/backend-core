b3e2410b066d02adf956dfc99b449d98d5eed3d6
Oliver Sebastian <oliverrsebastian@gmail.com>
2021-04-16 18:07:38 +0700

    feat(jwt service): move all token logic to jwt service

---
+++ src/services/common/jwtService.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const jwtService = {};
+
+jwtService.sign = async (payload, expiresIn) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: expiresIn });
+}
+
+jwtService.verify = async (token, ignoreExpiration) => {
+    return new Promise((resolve, reject) => {
+        return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { ignoreExpiration: ignoreExpiration }, function (err, data) {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+
+}
+
+module.exports = jwtService;
