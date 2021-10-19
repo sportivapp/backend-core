b4b85e850b562b984ccfaceb83e0f082a4e0f62f
Surya <suryaboyz04@gmail.com>
2021-10-19 20:16:14 +0700

    Fix/static file path (#121)
    * dynamic path
    
    * unignore knexfile & firebase adminsdk
    
    * knexfile client & connection using env vars
    
    * typo env var

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,11 @@
+require('dotenv').config({path: '.env'})
 const path = require('path');
 const BASE_PATH = path.join(__dirname, 'src', 'db');
 
 module.exports = {
   development: {
     client: process.env.DATABASE_CLIENT,
-    connection: process.env.DATABASE_CONNECTION,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
