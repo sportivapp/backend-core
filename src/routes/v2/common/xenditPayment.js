ed658f1236c0fd20fd6e187a1f8a3a895eb2f79e
Surya <suryaboyz04@gmail.com>
2021-11-15 15:38:11 +0700

    Feat/xendit (#141)
    * add xendit service to generate payment with xendit
    
    * add invoiceUrl to xendit_payment table
    
    * add callback to receive payment notification from Xendit
    proceed payment to register paying user
    add xendit payment channels for Mobile/Web
    add more attributes(column) to xendit payment table
    install xendit-node
    
    * add administration fee price on new registrant
    
    * return url and invoice as object on paid trx
    
    * fix cannot convert
    
    * typo..
    
    * fixes for invoice duration, payment methods
    change logic to hit xendit before saving to DB
    npm install UUID
    
    * remove invoice_code from class_transaction
    
    * fix circular dependency
    
    * remove console log
    
    * uncomment firebase init
    
    * add check invoice API

--- src/routes/v2/common/xenditPayment.js
+++ src/routes/v2/common/xenditPayment.js
@@ -6,5 +6,6 @@ const controller = require('../../../controllers/v2/xenditPayment');
 
 router.get(routes.xendit.channels, auth.authenticateToken, controller.getPaymentChannels);
 router.post(routes.xendit.receivePayment, xenditPaymentAuth.checkCallbackToken, controller.receivePayment);
+router.post(routes.xendit.invoice, auth.authenticateToken, controller.getInvoiceStatus);
 
 module.exports = router.expressRouter;
