9d8187b4b896b7ac89c57962bbe5ca30d669442f
Surya <suryaboyz04@gmail.com>
2021-11-24 16:44:20 +0700

    Feat/xendit (#154)
    * add xendit service to generate payment with xendit
    
    * add invoiceUrl to xendit_payment table
    
    * add callback to receive payment notification from Xendit
    proceed payment to register paying user
    add xendit payment channels for Mobile/Web
    add more attributes(column) to xendit payment table
    install xendit-node
    
    * add administration fee price on new registrant
    
    * return url and invoice as object on paid trx
    
    * fix cannot convert
    
    * typo..
    
    * fixes for invoice duration, payment methods
    change logic to hit xendit before saving to DB
    npm install UUID
    
    * remove invoice_code from class_transaction
    
    * fix circular dependency
    
    * remove console log
    
    * uncomment firebase init
    
    * add check invoice API
    
    * add API to get awaiting payments
    
    * uncomment filter
    
    * add get payment detail
    
    * add +7hours (wib)
    
    * add name to get awaiting payments
    
    * add registered checking logic
    
    * uncomment
    
    * fix increment subtotal (reset to 0 every trx)
    
    * uncomment
    
    * return time limit on query
    
    * no need to add admin fee again.
    
    * change to add admin fee to total price
    outside of processItems
    
    * added invoiceUrl on get awaiting payments
    
    * record payment method when register

--- src/services/v2/xenditPaymentService.js
+++ src/services/v2/xenditPaymentService.js
@@ -82,6 +82,7 @@ xenditPaymentService.createXenditPayment = async (invoice, amount, description, 
             xenditInvoiceId: xenditResponse.id,
             invoiceUrl: xenditResponse.invoiceUrl,
             paymentDetails: JSON.stringify(paymentDetails),
+            paymentMethod: paymentChannel,
         }
 
         const xenditPayment = await XenditPayment.query(trx)
@@ -180,6 +181,7 @@ xenditPaymentService.getAwaitingPayments = async (user) => {
             price: parseInt(awaitingPayment.amount),
             expiryDate: awaitingPayment.expiryDate,
             paymentType: paymentType,
+            invoiceUrl: awaitingPayment.invoiceUrl,
         }
     })
 }
