5e48a5dd149ce7ec8157761058b89dbca744ccef
Staticerz <suryaboyz04@gmail.com>
2021-11-26 15:48:54 +0700

    should be - to negate the effect of +7 tz

--- src/models/v2/ClassCategory.js
+++ src/models/v2/ClassCategory.js
@@ -61,15 +61,15 @@ class ClassCategory extends Model {
                     .withGraphFetched('class(basic)')
                     .withGraphFetched('categorySessions(list) as ongoingSessions')
                     .modifyGraph('ongoingSessions', builder => {
-                        // why 7? we fix at utc+7
-                        builder.where('start_date', '>=', new Date().setHours(7, 0, 0, 0))
+                        // why -7? we fix at utc+7
+                        builder.where('start_date', '>=', new Date().setHours(-7, 0, 0, 0))
                             .where('status', sessionStatusEnum.ONGOING)
                             .orderBy('start_date', 'ASC');
                     })
                     .withGraphFetched('categorySessions(list) as upcomingSessions')
                     .modifyGraph('upcomingSessions', builder => {
-                        // why 7? we fix at utc+7
-                        builder.where('start_date', '>=', new Date().setHours(7, 0, 0, 0))
+                        // why -7? we fix at utc+7
+                        builder.where('start_date', '>=', new Date().setHours(-7, 0, 0, 0))
                             .where('status', sessionStatusEnum.UPCOMING)
                             .orderBy('start_date', 'ASC');
                     });
