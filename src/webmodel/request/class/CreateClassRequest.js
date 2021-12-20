78cdef5a6c65e7aecc83604a879c9065fe419a5a
Staticerz <suryaboyz04@gmail.com>
2021-12-20 15:25:41 +0700

    add validation for create class

---
+++ src/webmodel/request/class/CreateClassRequest.js
@@ -0,0 +1,26 @@
+const Joi = require('joi')
+
+const schema = Joi.object({
+    title: Joi.string().max(255).required(),
+    description: Joi.string().required(),
+    categories: Joi.array().items(Joi.object({
+        title: Joi.string().max(255).required(),
+        description: Joi.string().required(),
+        isRecurring: Joi.bool().required(),
+        price: Joi.number().min(0).when('isRecurring', { is: true, then: Joi.required(), otherwise: Joi.disallow() }),
+        sessions: Joi.array().when('isRecurring', {
+            is: false,
+            then: Joi.array().items(Joi.object({
+                startDate: Joi.number().required(),
+                endDate: Joi.number().required(),
+                price: Joi.number().min(0).required()
+            })),
+            otherwise: Joi.array().items(Joi.object({
+                startDate: Joi.number().required(),
+                endDate: Joi.number().required()
+            }))
+        })
+    }))
+})
+
+module.exports = schema
