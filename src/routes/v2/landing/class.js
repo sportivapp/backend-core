7be6efe67fd6be312fe9e5f34a9dd1081e8c804e
Staticerz <suryaboyz04@gmail.com>
2022-01-18 20:49:43 +0700

    my class change route

--- src/routes/v2/landing/class.js
+++ src/routes/v2/landing/class.js
@@ -5,7 +5,7 @@ const controller = require('../../../controllers/v2/landingClass');
 
 router.get(routes.classV2.list, auth.authenticateToken, controller.getClasses);
 router.post(routes.classV2.list, auth.authenticateToken, controller.createClass);
-router.get(routes.classV2.myCreatedClass, auth.authenticateToken, controller.getMyCreatedClasses);
+router.get(routes.classV2.myClass, auth.authenticateToken, controller.getMyCreatedClasses);
 router.get(routes.classV2.id, auth.authenticateToken, controller.getLandingClassDetail);
 
 module.exports = router.expressRouter;
