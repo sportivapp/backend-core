c486d4237282cc4913d35416548c9fc7eaba58a6
Staticerz <suryaboyz04@gmail.com>
2024-06-16 11:33:28 +0700

    push knexfile

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const BASE_PATH = path.join(__dirname, 'src', 'db');
+
+module.exports = {
+  development: {
+    client: 'postgres',
+    connection: 'postgres://postgres:postgres@localhost:5432/sportiv',
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    }
+  },
+  production: {
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL + `?ssl=true`,
+      migrations: {
+        directory: path.join(BASE_PATH, 'migrations')
+      },
+      useNullAsDefault: true
+    }
+  }
+};
